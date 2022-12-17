ebf2793c3b746120f8a2ee1141b19a07bf6363ce
Костя <Kostya@192.168.1.49>
2022-12-17 18:32:39 +0700

    add-yandex-metrica

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -12,16 +12,18 @@ export default function Layout({ children }) {
             {/* Подключение метрики и других надстроек */}
 
             <Script id="yandexMetrica" type="text/javascript" >
-                {/* {`(function(m,e,t,r,i,k,a){m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
-   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                {`(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();
+   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-        ym(88523540, "init", {
-          clickmap:true,
+   ym(91733859, "init", {
+        clickmap:true,
         trackLinks:true,
         accurateTrackBounce:true,
         webvisor:true
-   });`} */}
+   });`}
 
                 {`window.replainSettings = { id: '05d73dd1-2129-4bc9-b8d1-1792a63847e4' };
 (function(u){var s=document.createElement('script');s.async=true;s.src=u;
